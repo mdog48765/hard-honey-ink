41099733db74619316ea9de3f756995dc880afc3
mdog48765 <michaelkylemusic@hotmail.com>
2026-05-19 17:20:44 -0500

    removed vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -24,7 +23,6 @@ export default function App() {
         </Routes>
         <Footer />
       </Router>
-      <Analytics />
     </div>
   );
 }
