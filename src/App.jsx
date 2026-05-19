4fd78cf575c35b9c0ef5c7b1c674bff8d8ddb57c
mdog48765 <michaelkylemusic@hotmail.com>
2026-05-19 16:48:34 -0500

    debugging analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -7,7 +9,7 @@ import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Booking from "./pages/Booking";
 import "@fontsource/great-vibes";
-import { Analytics } from "@vercel/analytics/react";
+
 
 export default function App() {
   return (
