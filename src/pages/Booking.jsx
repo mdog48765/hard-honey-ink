f7d9a4521085127043b9d2cfbcf17833419b60a2
mdog48765 <michaelkylemusic@hotmail.com>
2026-05-20 20:42:37 -0500

    fixed location information

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -51,12 +51,12 @@ export default function Booking() {
                   Location
                 </span>
                 <a
-                  href="https://www.google.com/maps/search/?api=1&query=403+S+Main+St+Jacksonville+IL+62650"
+                  href="https://maps.app.goo.gl/d2CcvbksLje9449F6"
                   target="_blank"
                   rel="noreferrer"
                   className="text-lg text-[--hh-ink] transition-colors hover:text-[--hh-honey]"
                 >
-                  403 S Main St
+                  405 S Main St
                   <br />
                   Jacksonville, IL 62650
                 </a>
@@ -72,8 +72,7 @@ export default function Booking() {
           <div className="overflow-hidden rounded-2xl border border-[--hh-border] bg-[--hh-paper-soft] shadow-sm">
             <iframe
               title="Hard Honey Ink Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.3903946888004!2d-90.23210682402275!3d39.73088067155815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87dfebfc258affe5%3A0xce9c6d515c2a7f08!2s403%20S%20Main%20St%2C%20Jacksonville%2C%20IL%2062650!5e0!3m2!1sen!2sus!4v1779141928626!5m2!1sen!2sus"
-              width="100%"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.3931854492275!2d-90.23209962402277!3d39.730817971558125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87dfebfc25952fb5%3A0xf135216e8398b9b9!2s405%20S%20Main%20St%2C%20Jacksonville%2C%20IL%2062650!5e0!3m2!1sen!2sus!4v1779326522438!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"              width="100%"
               height="100%"
               style={{ border: 0, minHeight: "460px" }}
               allowFullScreen=""
